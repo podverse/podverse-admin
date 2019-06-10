ca8ce54a0e8035aa201ea7334f0c4a691bee7cad
Mitch Downey <mitchldowney@gmail.com>
2019-06-09 19:41:45 -0500

    Update create_groups.py

--- podverse_admin/podverse_db/management/commands/create_groups.py
+++ podverse_admin/podverse_db/management/commands/create_groups.py
@@ -7,6 +7,13 @@ class Command(BaseCommand):
     admin, adminCreated = Group.objects.get_or_create(name='Admin')
     curator, curatorCreated = Group.objects.get_or_create(name='Curator')
 
+    episodeCT = ContentType.objects.get(model='episode')
+    episodePerms = Permission.objects.filter(content_type=episodeCT)
+
+    for perm in episodePerms:
+        admin.permissions.add(perm)
+        curator.permissions.add(perm)
+
     podcastCT = ContentType.objects.get(model='podcast')
     podcastPerms = Permission.objects.filter(content_type=podcastCT)
     
@@ -14,12 +21,17 @@ class Command(BaseCommand):
         admin.permissions.add(perm)
         curator.permissions.add(perm)
 
-    episodeCT = ContentType.objects.get(model='episode')
-    episodePerms = Permission.objects.filter(content_type=episodeCT)
+    groupCT = ContentType.objects.get(model='group')
+    groupPerms = Permission.objects.filter(content_type=groupCT)
 
-    for perm in episodePerms:
+    for perm in groupPerms:
         admin.permissions.add(perm)
-        curator.permissions.add(perm)
+
+    userCT = ContentType.objects.get(model='user')
+    userPerms = Permission.objects.filter(content_type=userCT)
+
+    for perm in userPerms:
+        admin.permissions.add(perm)
 
     def handle(self, *args, **options):
         self.stdout.write("Groups created.")
