a93f10f2b402cd2cef683eaca06bbb23b6a95eed
Mitch Downey <mitchldowney@gmail.com>
2019-07-05 05:59:22 -0500

    Enable creating users through the interface

--- podverse_db/admin/userAdmin.py
+++ podverse_db/admin/userAdmin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from podverse_db.models import User
+import bcrypt
 
 class UserAdmin(admin.ModelAdmin):
-    fields = ('id', 'email', 'emailVerified', 'freeTrialExpiration', 'isPublic', 'membershipExpiration',
-        'name', 'roles', 'createdAt', 'updatedAt',)
+    user_fields = ['id', 'email', 'emailVerified', 'freeTrialExpiration', 'isPublic', 'membershipExpiration',
+        'name', 'roles', 'createdAt', 'updatedAt',]
+
+    fields = user_fields
     list_display = ('id', 'email', 'name',)
     ordering = ('-updatedAt',)
     search_fields = ('id', 'email', 'name')
@@ -11,8 +14,22 @@ class UserAdmin(admin.ModelAdmin):
     def get_readonly_fields(self, request, obj=None):
         fields = [f.name for f in self.model._meta.fields]
         if request.user.is_superuser:
-            return ['createdAt', 'updatedAt',]
+            return ['createdAt', 'updatedAt']
         else:
             return fields
+
+    def get_form(self, request, obj=None, **kwargs):
+        if request.user.is_superuser:
+            superuser_fields = self.user_fields + ['password']
+            self.fields = superuser_fields
+        else:
+            self.fields = self.user_fields
+            self.exclude = ['password']
+        form = super(UserAdmin, self).get_form(request, obj, **kwargs)
+        return form
+
+    def save_model(self, request, obj, form, change):
+        obj.password = bcrypt.hashpw(obj.password, bcrypt.gensalt())
+        super(UserAdmin, self).save_model(request, obj, form, change)
 
 admin.site.register(User, UserAdmin)
--- podverse_db/models/user.py
+++ podverse_db/models/user.py
@@ -1,8 +1,12 @@
-from django.contrib.postgres.fields import ArrayField
+from django.contrib.postgres.fields import ArrayField, JSONField
 from django.db import models
+import shortuuid
 
 class User(models.Model):
-    id = models.CharField(max_length=14, primary_key=True)
+    def shortid():
+        return shortuuid.ShortUUID().random(length=7)
+
+    id = models.CharField(max_length=14, primary_key=True, default=shortid)
 
     email = models.EmailField(unique=True)
     emailVerified = models.BooleanField()
@@ -10,11 +14,15 @@ class User(models.Model):
     isPublic = models.BooleanField(default=False)
     membershipExpiration = models.DateTimeField(blank=True)
     name = models.CharField(max_length=2084, blank=True)
+    password = models.CharField(max_length=2084)
     roles = ArrayField(
         models.CharField(max_length=2084),
         size=20,
-        default=list
+        default=list,
+        blank=True
     )
+    historyItems = JSONField(default=list, blank=True)
+    queueItems = JSONField(default=list, blank=True)
 
     createdAt = models.DateTimeField(auto_now_add=True)
     updatedAt = models.DateTimeField(auto_now=True)
