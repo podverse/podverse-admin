049b760bc9e51c2fa552239940ac8a05fa08b9dd
Mitch Downey <mitchldowney@gmail.com>
2019-08-11 18:24:08 -0500

    Add autocomplete field for feedUrl.podcast

--- podverse_db/admin/feedUrlAdmin.py
+++ podverse_db/admin/feedUrlAdmin.py
@@ -7,6 +7,7 @@ class FeedUrlAdmin(admin.ModelAdmin):
     list_editable = ('isAuthority',)
     ordering = ('-updatedAt',)
     search_fields = ('id', 'url',)
+    autocomplete_fields = ('podcast',)
 
     def get_podcast_title(self, obj):
         return obj.podcast.title
