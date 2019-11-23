f36632fd2f99fb9b7d6ed1fd8df862ef991325fb
Mitch Downey <mitchldowney@gmail.com>
2019-11-22 23:12:39 -0600

    Update emailTemplate.py

--- podverse_admin/scripts/emailTemplate.py
+++ podverse_admin/scripts/emailTemplate.py
@@ -16,6 +16,8 @@ def emailTemplate(obj):
 
         <head>
             <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width">
+            <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
             <title>Podverse</title>
             <style>
             body {{
