9bdc6635ba1d98801cd7adb0f10f75346c83cf53
Mitch Downey <mitchldowney@gmail.com>
2020-04-25 20:50:07 -0500

    Remove email from accountClaimToken

--- podverse_db/admin/accountClaimToken.py
+++ podverse_db/admin/accountClaimToken.py
@@ -3,10 +3,10 @@ from django.contrib import admin
 from podverse_db.models import AccountClaimToken
 
 class AccountClaimTokenAdmin(admin.ModelAdmin):
-    fields = ('id', 'claimed', 'email', 'yearsToAdd', 'createdAt', 'updatedAt',)
-    list_display = ('id', 'claimed', 'email', 'yearsToAdd',)
+    fields = ('id', 'claimed', 'yearsToAdd', 'createdAt', 'updatedAt',)
+    list_display = ('id', 'claimed', 'yearsToAdd',)
     ordering = ('-updatedAt',)
-    search_fields = ('id', 'email',)
+    search_fields = ('id',)
 
     def get_readonly_fields(self, request, obj=None):
         fields = [f.name for f in self.model._meta.fields]
@@ -14,7 +14,6 @@ class AccountClaimTokenAdmin(admin.ModelAdmin):
             return ['id', 'createdAt', 'updatedAt']
         elif request.user.groups.filter(name='Admin').exists():
             fields.remove('claimed')
-            fields.remove('email')
             fields.remove('yearsToAdd')
             return fields
         else:
--- podverse_db/models/accountClaimToken.py
+++ podverse_db/models/accountClaimToken.py
@@ -4,7 +4,6 @@ from django.db import models
 class AccountClaimToken(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     claimed = models.BooleanField(default=False)
-    email = models.CharField(max_length=2084)
     yearsToAdd = models.PositiveIntegerField(default=0)
     createdAt = models.DateTimeField(auto_now_add=True)
     updatedAt = models.DateTimeField(auto_now=True)
